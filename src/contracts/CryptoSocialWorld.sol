38d6c51f513a394322b8786fee80e300c4048515
Tyler <53495442+nuvallo@users.noreply.github.com>
2021-12-30 17:03:56 -0500

    image state created

--- src/contracts/CryptoSocialWorld.sol
+++ src/contracts/CryptoSocialWorld.sol
@@ -4,9 +4,21 @@ contract CryptoSocialWorld {
     // state stored on the blockchain
     string public name = "CryptoSocialWorld";
 
-    // Store Posts
+    // Stores Images
+    mapping(uint256 => Image) public images;
+
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable auther;
+    }
 
     // Create Posts
+    function uploadImage() public {
+        images[1] = Image(1, "abc123", "Hello, world!", 0, address(0x0));
+    }
 
     // Tip Posts
 }
